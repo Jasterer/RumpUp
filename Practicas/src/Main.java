dddd578667d638ede5f24b85927c5431f80cc0ec
Ignacio <ignaciogomezsanchez@outlook.fr>
2020-04-28 19:29:17 +0200

    Primer exercicio suma de dos enteros

--- Practicas/src/Main.java
+++ Practicas/src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
+// Comentario de una sola linea
+    /*
+      Commentario de varias
+      lineas
+      que puedes
+      escribir como Kapo
 
+     */
     public static void main(String[] args){
 
         System.out.println("Hello World");
+        // Suma dos enteros
+        // 5+3 y came la salid por la consola
 
     }
 }
