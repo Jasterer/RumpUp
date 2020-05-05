ebbf819b6bb72babc998b1f426c57403fac15b31
Jasterer <joni.gfgh@gmail.com>
2020-05-05 21:58:43 +0200

    primer commit

--- Practicas/src/Main.java
+++ Practicas/src/Main.java
@@ -1,171 +1,53 @@
 import java.util.Scanner;
 
 public class Main {
-    // Comentario de una sola linea
-    /*
-      Commentario de varias
-      lineas
-      que puedes
-      escribir como Kapo
 
-     */
+
     public static void main(String[] args) {
+        Matematicas mates = new Matematicas();
 
-        // Suma dos enteros
-        // 5+3 y dame la salida por la consola
+        Scanner operacion = new Scanner(System.in);
+        System.out.println("introduzca la operación a realizar : \n" +
+                " 1 suma \n" +
+                " 2 resta \n" +
+                " 3 multiplicación \n" +
+                " 4 división");
+        int ope = operacion.nextInt();
+        System.out.println("introduzca parametro 1");
+        int param1 = operacion.nextInt();
+        System.out.println("introduzca parametro 2");
+        int param2 = operacion.nextInt();
 
-        int num1=5;
-        int num2=3;
+        if (ope == 1) {
+            int suma = mates.sumas(param1, param2);
+            System.out.println("la suma es : " + suma);
+        }
 
-        int resultado;
-        resultado=num1+num2;
-        System.out.println("El resultado de la suma es igual a: "+ resultado);
+        if (ope == 2) {
+            int resta = mates.restas(param1, param2);
+            System.out.println("la resta es : " + resta);
+        }
 
-        //Lectura analítica de la suma
+        if (ope == 3) {
+            int multiplicación = mates.multiplicas(param1, param2);
+            System.out.println("la multiplicación es : " + multiplicación);
+        }
+
+        if (ope == 4) {
+            int división = mates.divides(param1, param2);
+            System.out.println("la división es : " + división);
+        }
 
 
-        num1=leeUnSumando();
-        num2=leeUnSumando();
-
-        SumaMatematicas calculadora = new SumaMatematicas();
-        resultado = calculadora.sumaDosNumeros(num1,num2);
-        System.out.println("El resultado de la suma es igual a: "+ resultado);
-
-      //  sumaDosNumeros();
-
-        System.out.println("El resultado de la suma es igual a: "+ calculadora.sumaDosNumeros(num1,num2));
     }
-    /**
-     * Funcion que recoge una entrada
-     * @return el valor
-     *
-     */
+
+
     public static int leeUnSumando() {
         int param1;
-        Scanner objeto=new Scanner(System.in);
+        Scanner objeto = new Scanner(System.in);
         System.out.println("introduzca el primer número de la suma");
-        param1=objeto.nextInt();
+        param1 = objeto.nextInt();
         return param1;
     }
 
-
-
-
-    /**
-     * Funcion estática original
-     */
-    public static void sumaDosNumeros() {
-
-        int num1=0,num2=0, resultado=0;
-
-        Scanner objeto=new Scanner(System.in);
-
-        System.out.println("introduzca el primer número de la suma");
-        num1=objeto.nextInt();
-
-        System.out.println("Introduzca e segundo número de la suma");
-        num2=objeto.nextInt();
-
-        resultado=num1+num2;
-
-        System.out.println("El resultado de la suma es igual a:"+resultado);
-
-    }
 }
-
-
-/**  import java.util.Scanner;
-
- public class Main {
-
-     public static int main(String[] args) {
-
-       suma=SumaMatematicas;
-       resta=RestaMatematicas;
-       multiplicacion=multiplacionMatematicas;
-       division=DivisionMatematicas;
-       Scanner operacion=new Scanner(System.in);
-       System.out.println("introduzca la operación a realiza:(suma,resta,multiplicación o división)");
-       ope=operacion.nextInt();
-       }
-
-     public static void main(String[] args) {
-       if ope=suma=SumaMatematicas;
-         num1=leeUnSumando();
-         num2=leeUnSumando();
-
-         SumaMatematicas calculadora = new SumaMatematicas();
-         resultado = calculadora.sumaDosNumeros(num1,num2);
-         System.out.println("El resultado de la suma es igual a: "+ resultado);
-
-         System.out.println("El resultado de la suma es igual a: "+ calculadora.sumaDosNumeros(num1,num2));
-         }
-
-     public static int leeUnSumando() {
-         int param1;
-         Scanner objeto=new Scanner(System.in);
-         System.out.println("introduzca el primer número de la suma");
-         param1=objeto.nextInt();
-         return param1;
-         }
-
-
-     public static void main(String[] args) {
-       if ope=resta=RestaMatematicas;
-         num1=leeUnSumando();
-         num2=leeUnSumando();
-
-         RestaMatematicas calculadora = new restaMatematicas();
-         resultado = calculadora.restaDosNumeros(num1,num2);
-         System.out.println("El resultado de la resta es igual a: "+ resultado);
-
-         System.out.println("El resultado de la resta es igual a: "+ calculadora.sumaDosNumeros(num1,num2));
-         }
-
-      public static int leeUnSumando() {
-         int param1;
-         Scanner objeto=new Scanner(System.in);
-         System.out.println("introduzca el primer número de la resta");
-         param1=objeto.nextInt();
-         return param1;
-         }
-
-     public static void main(String[] args) {
-      if ope=multiplicación=multiplicacionMatematicas;
-         num1=leeUnSumando();
-         num2=leeUnSumando();
-
-         MultiplicacionMatematicas calculadora = new multiplicacionMatematicas();
-         resultado = calculadora.multiplicacionDosNumeros(num1,num2);
-         System.out.println("El resultado de la multiplicación es igual a: "+ resultado);
-
-         System.out.println("El resultado de la multiplicación es igual a: "+ calculadora.sumaDosNumeros(num1,num2));
-         }
-
-     public static int leeUnSumando() {
-         int param1;
-         Scanner objeto=new Scanner(System.in);
-         System.out.println("introduzca el primer número de la multiplicación");
-         param1=objeto.nextInt();
-         return param1;
-         }
-
-     public static void main(String[] args) {
-      if ope=división=DivisionMatematicas;
-         num1=leeUnSumando();
-         num2=leeUnSumando();
-
-         DivisionMatematicas calculadora = new DivisionMatematicas();
-         resultado = calculadora.divisionDosNumeros(num1,num2);
-         System.out.println("El resultado de la división es igual a: "+ resultado);
-
-         System.out.println("El resultado de la división es igual a: "+ calculadora.divisionDosNumeros(num1,num2));
-         }
-
-     public static int leeUnSumando() {
-         int param1;
-         Scanner objeto=new Scanner(System.in);
-         System.out.println("introduzca el primer número de la división");
-         param1=objeto.nextInt();
-         return param1;
-         }
---
+++ Practicas/src/Matematicas.java
@@ -0,0 +1,20 @@
+public class Matematicas {
+    public int sumas(int param1, int param2) {
+        return (param1 + param2);
+    }
+
+    public int divides(int param1, int param2) {
+        int resultado = param1 / param2;
+        return resultado;
+    }
+
+    public int multiplicas(int param1, int param2) {
+        int resultado = param1 * param2;
+        return resultado;
+    }
+
+    public int restas(int param1, int param2) {
+        int resultado = param1 - param2;
+        return resultado;
+    }
+}
